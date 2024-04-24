d55cd51d07bf72ff54a4c507b9221523e4371661
unknown <carlosaguilar3rd@gmail.com>
2024-04-24 16:13:10 +0900

    changed java version to 1.8 and deleted webSocketClient

--- src/main/java/helper/OrderBookHelper.java
+++ src/main/java/helper/OrderBookHelper.java
@@ -1,26 +1,8 @@
 package helper;
 
-import client.websocket.BinanceWebSocketClient;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class OrderBookHelper {
-    public static void main(String[] args) throws URISyntaxException {
-        BinanceWebSocketClient client = new BinanceWebSocketClient(new URI("wss://stream.binance.com:9443/stream?streams=btcusdt@depth/ethusdt@depth"));
-        client.connect();
-
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-
-        executorService.scheduleAtFixedRate(() -> {
-            // Display the latest message every 10 seconds
-            System.out.println("Latest message: " + client.getBinanceMessage());
-        }, 0, 10, TimeUnit.SECONDS);
-    }
 
     public static double computeVolumeAmount(List<Object> bidList, List<Object> askList) {
         double bidTotal = 0;
